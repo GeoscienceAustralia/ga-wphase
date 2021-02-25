e1cce895e36f4fa80985d31eea1ce6d331955e21
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-02-25 14:38:01 +1100

    Failing test to illustrate issue with dup packets
    We ran a playback exercise using a miniseed dataset that had some
    duplicate packets, which caused obspy's data loader to generate many
    traces per channel. The current w-phase code interprets this as gappy
    data and throws it all out.
    
    Required a small refactor to expose the gap removal for testing.

---
+++ tests/test_remove_gappy_traces.py
@@ -0,0 +1,9 @@
+import os
+import obspy
+DATADIR = os.path.join(os.path.dirname(__file__), 'waveforms')
+
+def test_can_handle_data_with_duplicate_packets():
+    from wphase.psi.inventory import remove_gappy_traces
+    st0 = obspy.read(DATADIR + '/duplicates.mseed')
+    st1 = remove_gappy_traces(st0)
+    assert len(set(tr.meta.station for tr in st1)) == 4

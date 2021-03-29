9483aff27a22958ec61f97d58e6bdc99758ffb65
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-03-30 09:39:15 +1100

    Remove dead code

--- wphase/psi/seismoutils.py
+++ wphase/psi/seismoutils.py
@@ -24,94 +24,6 @@ except ImportError:
 
 from . import datautils as DU
 
-def getMetadataFromSeed(sp, trid, datetime=None):
-    '''
-    Given a parser object from a dataless seed and a station id , this function returns a
-    dictionary containing every station id and: Latitude, Longitude, Type of Transfer function,
-    Elevation, Azimuth, Dip, poles, zeros, gain (normalization factor) and sensitivity.
-
-    PS: This function is based on getPAZ and getCoordinates of OBSPY but it adds more
-    information to the output. Note that transfer function is specified.
-    '''
-
-    blks = sp._select(trid,datetime)
-    poles = None
-    for blk in blks:
-        if blk.id == 58:
-                if blk.stage_sequence_number == 0:
-                    sens = blk.sensitivity_gain
-                elif blk.stage_sequence_number == 1:
-                    seisgain = blk.sensitivity_gain
-                elif blk.stage_sequence_number == 2:
-                    diggain = blk.sensitivity_gain
-
-
-
-        if blk.id == 52:
-            lat = blk.latitude
-            lon = blk.longitude
-            Elev= blk.elevation
-            azi = blk.azimuth
-            dip = blk.dip
-
-        if blk.id == 53 and poles == None:
-            tf = blk.transfer_function_types
-            gain = blk.A0_normalization_factor
-            poles = []
-            for i in range(blk.number_of_complex_poles):
-                try:
-                    p = complex(blk.real_pole[i], blk.imaginary_pole[i])
-                except TypeError:
-                    p = complex(blk.real_pole, blk.imaginary_pole)
-                poles.append(p)
-
-            zeros = []
-            for i in range(blk.number_of_complex_zeros):
-                try:
-                    z = complex(blk.real_zero[i], blk.imaginary_zero[i])
-                except TypeError:
-                    z = complex(blk.real_zero, blk.imaginary_zero)
-                zeros.append(z)
-
-
-
-
-    metadata   =  { 'latitude'       : lat,
-                     'longitude'      : lon,
-                     'elevation'      : Elev,
-                     'transfer_function': tf,
-                     'azimuth'        : azi,
-                     'dip'            : dip,
-                     'zeros'          : zeros,
-                     'poles'          : poles,
-                     'gain'           : gain,
-                     'sensitivity'    : sens
- #                   'seismometer_gain': seisgain,
-  #                   'digitizer_gain' : diggain
-                   }
-
-    return metadata
-
-
-def hypo2dist(hyploc1,hyploc2,r=6371.):
-    '''
-    Given 2 tuples with hypcenter coordinates (lat, lon, depth) returns the
-    distance in KM between them.
-    '''
-
-    r1 = r-hyploc1[2]
-    r2 = r-hyploc2[2]
-    torad = np.pi/180.
-    #dellon = np.abs(torad*(hyploc1[1]-hyploc2[1]))
-    theta = torad*locations2degrees(hyploc1[0],hyploc1[1],\
-                                         hyploc2[0],hyploc2[1])
-    #theta2 = np.arccos(np.sin(torad*hyploc1[0])*np.sin(torad*hyploc2[0])
-    #               + np.cos(torad*hyploc1[0])*np.cos(torad*hyploc2[0])*np.cos(dellon))
-    ##Law of Cosines:
-    l = np.sqrt(r1**2.+r2**2.-2.*r2*r1*np.cos(theta))
-    #l2 = np.sqrt(r1**2.+r2**2.-2.*r2*r1*np.cos(theta2))
-    return l#,l2
-
 
 
 def AzimuthalGap(META,trlist, location):
@@ -144,45 +56,6 @@ def AzimuthalGap(META,trlist, location):
     return  gap, azi_cov
 
 
-def GetVirtualNetworkStations(VN):
-    '''
-    Given a Virtual Network (e.g. "FDSN") it returns
-    a list with the stations in the Network.
-    '''
-
-    url = "http://service.iris.edu/irisws/virtualnetwork/1/query?code=_" + VN +"&format=CSV"
-    #url = "http://www.iris.edu/vnets?vnet=_" + VN +"&vout=CSV"
-    lines = urllib.request.urlopen(url).read().split("\n")
-    stations = []
-    for line in lines:
-        if line.startswith('_'):
-            fields = line.rstrip().split(',')
-            net = fields[1]
-            sta = fields[2]
-            stations.append(net + "." + sta)
-
-    return stations
-
-def resample_Ntraces(Ntraces, DecFac):
-    '''
-    This function recomputes the proper length of each trace (given as a
-    Ntraces list) after we applied a decimation to the full array.
-    '''
-
-    Ntraces_dec = np.empty(len(Ntraces))
-    res = 0.
-    new_val = Ntraces[0]
-    if np.any(np.array(Ntraces) <= DecFac):
-        logger.warning("At least one trace is smaller that the Decimation Factor")
-    for i in range(len(Ntraces)):
-        if not i == 0:
-            res = round((math.ceil(new_val/DecFac)
-                        - new_val/DecFac)*DecFac)
-        new_val = Ntraces[i]-res
-        Ntraces_dec[i] = math.ceil(new_val/DecFac)
-    return Ntraces_dec
-    ##
-
 def station_pruning(META,trlist, cutoffs=[1.,2.,5], units='deg'):
     '''
     Given a list of id trlist and its respective metadata dictionary META, it
@@ -214,35 +87,4 @@ def station_pruning(META,trlist, cutoffs=[1.,2.,5], units='deg'):
                     except Exception:
                         pass
         trlist = list(red_trlist)# copying reduced list
-    return   trlist
-
-
-def get_new_coordinates(lat1, lon1, azi, dist):
-    '''computes the location (lat,lon) of a new point given the a location and a
-       distance, azimuth pair
-       UPDATE: I think this is not a right approach. The Azimimuth will not project
-               as I thought. Better to use  geographiclib.
-    '''
-
-    r = 6371.
-    dist_rad = np.pi/180.*dist
-    azi_rad = np.pi/180.*azi
-    lat1_rad = np.pi/180.*lat1
-    lon1_rad = np.pi/180.*lon1
-
-    dp = np.sqrt(2*r**2*(1-np.cos(dist_rad)))
-    y = dp*np.cos(azi_rad)
-    x = dp*np.sin(azi_rad)
-
-    dlat = np.arccos(1-(y**2)/(2*r**2))
-    dlon = np.arccos(1-(x**2)/(2*r**2))
-    if azi > 90 and azi < 270:
-        dlat = -dlat
-    if azi > 180 and azi < 360:
-        dlon = -dlon
-
-    lat2_rad = dlat + lat1_rad
-    lon2_rad = dlon + lon1_rad
-
-    return 180/np.pi*lat2_rad, 180/np.pi*lon2_rad
-
+    return  trlist

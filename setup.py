18936845a5e96b933ad54f118d5991ab5e12a5f3
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-06-03 10:40:03 +1000

    0.2 release candidate

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ else:
 
 setup(
     name = 'Wphase',
-    version = '0.2',
+    version = '0.2rc1',
     packages = ['wphase', 'wphase.psi'],
     author = 'Geoscience Austraila',
     description = 'Wphase calculations and web interface.',

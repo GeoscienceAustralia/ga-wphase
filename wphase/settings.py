11599288b1e61e62cdb23a08ca55650a6e2b5f78
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-07-29 11:36:09 +1000

    Make important settings configurable via env vars.

--- wphase/settings.py
+++ wphase/settings.py
@@ -4,10 +4,8 @@
 .. module:: settings
 
 Configuration settings for :mod:`wphase`. At present, the settings are declared
-directly as python variables, but eventually may be moved to an external file
-(e.g. JSON) which persists outside the code base and can be customised for a
-specific system.
-"""
+directly as python variables, but those that should be changed by users can
+also be overridden with environment variables."""
 
 from __future__ import absolute_import
 
@@ -18,26 +16,48 @@ import multiprocessing as mp
 # Set the number of threads used by OpenBLAS.
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
-#: Use HDF5 version of Green's functions (the alternative [original] is to use
-#: SAC files).
-USE_HDF5_GREENS_DB = True
-
 #: Directory containing the green's functions.
 GREEN_DIR = os.environ['WPHASE_GREENS_FUNCTIONS']
 
 DEFAULT_OUTPUT_DIR = os.environ.get('WPHASE_OUTPUT_DIR', '/tmp/wphase-output')
 
-#: The number of worker processes to use in :py:class:`multiprocessing.Pool`.
-N_WORKERS_IN_POOL = None
+#: The number of worker processes to use when multiprocessing.
+N_WORKERS_IN_POOL = os.environ.get('WPHASE_WORKER_COUNT', None)
+if N_WORKERS_IN_POOL is not None:
+    N_WORKERS_IN_POOL = float(N_WORKERS_IN_POOL)
+
+#: Authority used for wphase.
+GA_AUTHORITY = os.environ.get('WPHASE_AUTHORITY', "GA W-phase")
+
+#: The name of the current host.
+WPHASE_HOST_NAME = os.environ.get('WPHASE_HOST_NAME', 'localhost')
+if not WPHASE_HOST_NAME:
+    raise Exception('env var WPHASE_HOST_NAME is defined but is blank')
+
+#: Should profiling information for wphase be produced.
+PROFILE_WPHASE = os.environ.get('WPHASE_PROFILE', 'false').lower() in ('1', 'true')
+
+#: Number of traces to put in each wphase results plot
+N_TRACES_PER_RESULT_PLOT = os.environ.get('WPHASE_N_TRACES_PER_RESULT_PLOT', 6)
+
+#: Implementation of bandpass filter to use
+BANDPASS_IMPLEMENTATION = os.environ.get('WPHASE_BANDPASS_IMPLEMENTATION', 'scipy')
+
+# Strength of regularization in preliminary magnitude calculation.
+# Higher values result in less weight being given to azimuthal variation - in
+# the limit at infinity, the azimuths are completely ignored.
+# 0.2 seems to work well enough to prevent issues without significantly
+# suppressing anisotropy in datasets with good azimuthal coverage.
+AMPLITUDE_REGULARIZATION = float(os.environ.get('WPHASE_AMPLITUDE_REGULARIZATION', 0.2))
+
+# Floor to clamp preliminary magnitude to
+MINIMUM_PRELIMINARY_MAGNITUDE = float(os.environ.get('WPHASE_MINIMUM_PRELIMINARY_MAGNITUDE', 6.5))
+
+
+### Filenames of output products:
 
 #: The name of the JSON file to contain the output from wphase caclcutions.
 WPHASE_OUTPUT_FILE_NAME = 'wphase_output.json'
-
-#: Name of the file that contains the exclude list.
-WPHASE_EXCLUDE_LIST_FILE_NAME = 'exclude_list.json'
-
-#: wphase intermediate mseed file name.
-WPHASE_MSEED_FILE = "Mini-SEED-traces.mseed"
 
 #: Prefix for the names of the images containing the beachball.
 WPHASE_BEACHBALL_PREFIX = "beachball"
@@ -60,21 +80,9 @@ WPHASE_MISFIT_PREFIX = "wphase_misfit"
 #: Prefix for the names of the images containing the preliminary magnitude fit
 WPHASE_PRELIM_FIT_PREFIX = "preliminary_magnitude_fit"
 
-#: Authority used for wphase.
-GA_AUTHORITY = "GA W-phase"
 
-#: The name of the current host.
-WPHASE_HOST_NAME = os.environ.get(
-    'WPHASE_HOST_NAME',
-    'localhost')
-if not WPHASE_HOST_NAME:
-    raise Exception('env var WPHASE_HOST_NAME is defined but is blank')
-
-#: Should profiling information for wphase be produced.
-PROFILE_WPHASE = False
-
-#: Number of traces to put in each wphase results plot
-N_TRACES_PER_RESULT_PLOT = 6
+### The remaining settings should probably not be changed - they define the
+### structure of the w-phase results JSON.
 
 #: Key for warnings in the result dictionary.
 WPHASE_WARNINGS_KEY = 'Warnings'
@@ -103,15 +111,3 @@ MISFIT_KEY = 'Misfits'
 #: Key containing the host name.
 HOST_NAME_KEY = 'HostName'
 
-#: Implementation of bandpass filter to use
-BANDPASS_IMPLEMENTATION = 'scipy'
-
-# Strength of regularization in preliminary magnitude calculation.
-# Higher values result in less weight being given to azimuthal variation - in
-# the limit at infinity, the azimuths are completely ignored.
-# 0.2 seems to work well enough to prevent issues without significantly
-# suppressing anisotropy in datasets with good azimuthal coverage.
-AMPLITUDE_REGULARIZATION = 0.2
-
-# Floor to clamp preliminary magnitude to
-MINIMUM_PRELIMINARY_MAGNITUDE = 6.5

4d39623ce56d321ee49400dd9a3e752bb23513fc
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-09-17 12:08:38 +1000

    Switch cartopy downloads to S3

--- docker/configure_wphase_image.sh
+++ docker/configure_wphase_image.sh
@@ -11,7 +11,25 @@ chmod +x /home/wphase/reinstall-wphase /home/wphase/reinstall-wphase-and-run
 # Do the build once to get dependencies installed.
 /home/wphase/reinstall-wphase
 
-# And we want to preload cartopy features:
+# Configure cartopy to fetch downloads from S3:
+usersitedir=$(python -c 'from __future__ import print_function; import site; print(site.getusersitepackages())')
+mkdir -p "$usersitedir"/cartopy_userconfig
+cat > "$usersitedir"/cartopy_userconfig/__init__.py <<'PYTHON'
+
+_SOURCE_TEMPLATE = 'https://naturalearth.s3.amazonaws.com/{resolution}_{category}/ne_{resolution}_{name}.zip'
+
+def update_config(config):
+    """Configures cartopy to download NaturalEarth shapefiles from S3 instead
+    of naciscdn."""
+    from cartopy.io.shapereader import NEShpDownloader
+    target_path_template = NEShpDownloader.default_downloader().target_path_template
+    downloader = NEShpDownloader(url_template=_SOURCE_TEMPLATE,
+                                 target_path_template=target_path_template)
+    config['downloaders'][('shapefiles', 'natural_earth')] = downloader
+
+PYTHON
+
+# Finally, pre-download the polygons we need:
 python3 - <<PYTHON
 from cartopy.feature import NaturalEarthFeature as NEF
 for feature in (('physical', 'coastline'),
